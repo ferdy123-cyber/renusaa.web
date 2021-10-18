4dce30f32094ddf9f5a643a808648b71e40a564b
ferdy <cepat9986@gmail.com>
2021-10-18 19:01:26 +0700

    hehe

--- pages/admin/project/index.js
+++ pages/admin/project/index.js
@@ -1,25 +1,45 @@
 import Layout from "../../../components/layout admin";
-import Image from "next/image";
-// import LazyLoad from "react-lazyload";
+import { Image } from "antd";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-const Project = (props) => {
-  const { projectData } = props;
+const Project = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://app.ferdyfian.xyz/portfolio")
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  // const getData = () => {};
 
-  console.log(projectData);
+  console.log(data);
   return (
     <Layout>
       <div className="">
         <h3>Project</h3>
-        {projectData.map((e) => {
+        {data.map((e) => {
           return (
-            <Image
-              key={e.id}
+            // <Image
+            //   key={e.id}
+            //   style={{ objectFit: "contain" }}
+            //   width={200}
+            //   height={200}
+            //   src={`https://docs.google.com/uc?id=${e.img_id}`}
+            //   alt=""
+            // />
+            <LazyLoadImage
               style={{ objectFit: "contain" }}
-              width={350}
-              height={350}
-              src={`https://docs.google.com/uc?id=${e.img_id}`}
               alt=""
-              placeholder="blur"
+              height={350}
+              src={`https://docs.google.com/uc?id=${e.img_id}`} // use normal <img> attributes as props
+              width={350}
+              effect="blur"
             />
           );
         })}
@@ -28,15 +48,15 @@ const Project = (props) => {
   );
 };
 
-export async function getData() {
-  const res = await fetch("https://app.ferdyfian.xyz/portfolio");
-  const data = await res.json();
+// export async function getServerSideProps() {
+//   const res = await fetch("https://app.ferdyfian.xyz/portfolio");
+//   const data = await res.json();
 
-  return {
-    props: {
-      projectData: data,
-    }, // will be passed to the page component as props
-  };
-}
+//   return {
+//     props: {
+//       projectData: data,
+//     }, // will be passed to the page component as props
+//   };
+// }
 
 export default Project;
